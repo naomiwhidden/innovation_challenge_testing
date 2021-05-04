1307c8b9785093cd18c003169b0d128949782b3b
purgat0ry <casey@camccarthy.com>
2021-05-04 14:11:27 +0900

    Baseline bootstrap template added.

--- iiimef_project/settings.py
+++ iiimef_project/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 SECRET_KEY = os.environ.get("DJ_SECRET_KEY", default='Testing')
-DEBUG = os.environ.get("DJ_DEBUG", default=False)
-ALLOWED_HOSTS = os.environ.get("DJ_ALLOWED_HOSTS").split(" ")
+DEBUG = os.environ.get("DJ_DEBUG", default=True)
+#ALLOWED_HOSTS = os.environ.get("DJ_ALLOWED_HOSTS")
 
 # Application definition
 
