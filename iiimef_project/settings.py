cafbe38e2f20e06fdfd7c8052339ec3add70f420
Lit Phansiri <lphansiri@gmail.com>
2021-05-04 20:17:01 +0900

    config docker-compose with aws mysql db

--- iiimef_project/settings.py
+++ iiimef_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("DJ_SECRET_KEY", default='Testing')
 DEBUG = os.environ.get("DJ_DEBUG", default=True)
-#ALLOWED_HOSTS = os.environ.get("DJ_ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.environ.get("DJ_ALLOWED_HOSTS")
 
 # Application definition
 
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'iiimef_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
