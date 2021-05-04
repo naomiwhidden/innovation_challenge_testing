8f8402682ef16bf8f20ce23e77719e5bb62b138d
purgat0ry <casey@camccarthy.com>
2021-05-04 23:59:41 +0900

    Units, Users, Locations and fixed model imports

--- redonkulator_app/models.py
+++ redonkulator_app/models.py
@@ -12,3 +12,30 @@ class Exercise(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+class Locations(models.Model):
+    location = models.CharField(max_length=45)
+
+    def __str__(self):
+        return f"{self.location}"
+
+class Users(models.Model):
+    email = models.CharField(max_length=45, unique=True)
+    edipi = models.IntegerField(primary_key=True)
+    display_name = models.CharField(max_length=45)
+    phone = models.CharField(max_length=45)
+
+
+    def __str__(self):
+        return f"User: {self.display_name}\tEDIPI: {self.edipi}\tEmail: {self.email}\tPhone: {self.phone}"
+
+class Units(models.Model):
+    uic = models.CharField(max_length=20, primary_key=True)
+    short_name = models.CharField(max_length=45, unique=True)
+    long_name = models.CharField(max_length=100, unique=True)
+    mcc = models.CharField(max_length=45, unique=True)
+    ruc = models.CharField(max_length=45, unique=True)
+    type = models.CharField(max_length=45, unique=True)
+
+    def __str__(self):
+        return f"UIC: {self.uic}\tName: {self.short_name}"
